5a4cd09e1769307df1b52c9dde42fe89703d0b81
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-05-24 15:12:46 +0300

    доопрацювання

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,13 @@
 const elementsList = document.querySelector(`#categories`);
-const itemsCategories = elementsList.querySelectorAll(`li.item`);
-console.log(`Number of categories: ${itemsCategories.length}`);
+const categoriesLength = elementsList.children.length;
+console.log(`Number of categories: ${categoriesLength}`);
 
+
+const itemsCategories = elementsList.querySelectorAll(`li.item`);
 itemsCategories.forEach(category => {
-    const titleCategory = category.querySelector(`h2`).textContent;
-    const elCategory = category.querySelectorAll(`li`).length;
-    console.log(`Category: ${titleCategory},Elements: ${elCategory}`)
+    console.log("Category: ", category.firstElementChild.textContent);
+  console.log("Elements: ", category.lastElementChild.children.length);
 })
+
+
+
--- js/task-08.js
+++ js/task-08.js
@@ -3,18 +3,21 @@ const { email, password } = loginForm.elements;
 
 
 loginForm.addEventListener('submit', onSubmit);
+const profile = {};
 
 function onSubmit(event) {
     event.preventDefault()
     console.log(event)
     console.log(email.value)
     console.log(password.value)
+
     if (email.value.trim() === "" || password.value.trim() === "") {
         alert("Please fill in all the fields!")
         alert("Please fill in all the fields!")
         }
         else {
         alert('Data send!')
+        console.log(profile)
            loginForm.reset()
         }
     }
